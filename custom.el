6e289bca4bf813bca757098e6489f089c787a9ed
Uwe Hubert <hubert@doctronic.de>
2018-10-10 15:20:35 +0200

    Colors

--- custom.el
+++ custom.el
@@ -5,8 +5,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   [zenburn-bg zenburn-red zenburn-green zenburn-yellow zenburn-blue zenburn-magenta zenburn-cyan zenburn-fg])
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (smart-mode-line-light)))
+ '(custom-safe-themes
+   (quote
+    ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(fci-rule-color "#393939")
  '(menu-bar-mode nil)
  '(org-agenda-files nil)
  '(package-selected-packages
@@ -28,4 +37,5 @@
  '(magit-item-highlight ((t (:background "gainsboro"))))
  '(markdown-code-face ((t (:inherit nil :foreground "dim gray" :box (:line-width 1 :color "grey25")))))
  '(mode-line ((t :foreground "black" :background "RosyBrown2" :inverse-video nil)))
- '(mode-line-inactive ((t :foreground "grey20" :background "Gainsboro" :inverse-video nil))))
+ '(mode-line-inactive ((t :foreground "grey20" :background "Gainsboro" :inverse-video nil)))
+ '(region ((t (:background "cyan" :distant-foreground "gtk_selection_fg_color")))))
