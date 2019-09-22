df82da56633ebf95be8fcc7be038aaccefbe7dd7
Uwe Hubert <hubert@doctronic.de>
2019-09-22 17:15:15 +0200

    with elpa

---
+++ elpa/ace-jump-buffer-0.4.1/ace-jump-buffer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-jump-buffer" "0.4.1" "fast buffer switching extension to `avy'" '((avy "0.4.0") (dash "2.4.0")) :commit "02797c22c10a817dbbdfbd8fddceeba6c4f0499a" :authors '(("Justin Talbott" . "justin@waymondo.com")) :maintainer '("Justin Talbott" . "justin@waymondo.com") :url "https://github.com/waymondo/ace-jump-buffer")
