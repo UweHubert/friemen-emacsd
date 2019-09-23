05092a306faf4ae8589f17c0fa2ff8b80a487395
Uwe Hubert <hubert@doctronic.de>
2019-09-23 13:12:46 +0200

    make gnu elpa https

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 ;; (setq url-proxy-services '(("http" . "YOUR-HOST:8080")))
 
 (setq package-archives
-      '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
+      '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
         ("MELPA Stable" . "https://stable.melpa.org/packages/")
         ("MELPA"        . "https://melpa.org/packages/"))
       package-archive-priorities
